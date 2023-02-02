7a4ae617931a87fe3fc92411a73132e814e83c20
IvanOrsh <ksironov@mail.ru>
2023-02-02 15:50:09 +0300

    minor fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -25,7 +25,7 @@ export class UsersController {
 
   @Get('/:id')
   async findUser(@Param('id') id: string) {
-    const user = this.usersService.findOne(parseInt(id));
+    const user = await this.usersService.findOne(parseInt(id));
     if (!user) {
       throw new NotFoundException('user not found');
     }
