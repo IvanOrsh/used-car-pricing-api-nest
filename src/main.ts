d0dca56cc61b6ca19c7f890a37bdf44c30f2456b
IvanOrsh <ksironov@mail.ru>
2023-02-03 15:31:23 +0300

    cookie-session middleware added

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const cookieSession = require('cookie-session');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cookieSession({
+      keys: ['brambowie'],
+    }),
+  );
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
