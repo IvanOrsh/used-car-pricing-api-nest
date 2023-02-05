e221aa3b73b55d90ee63cd248e5221fbdcc08dd3
IvanOrsh <ksironov@mail.ru>
2023-02-05 03:25:26 +0300

    Implementing GET /reports route handler - GetEstimateDto created - WIP: route handler & service

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,6 +5,8 @@ import {
   Patch,
   Param,
   UseGuards,
+  Get,
+  Query,
 } from '@nestjs/common';
 
 import { CreateReportDto } from './dtos/create-report.dto';
@@ -16,10 +18,16 @@ import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize-interceptor';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    // not yet
+  }
 
   @Post()
   @UseGuards(AuthGuard)
